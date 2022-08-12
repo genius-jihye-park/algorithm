a09682e3874de4589fc376324da84e975095b547
jihye_Park <sconenomad@gmail.com>
2022-08-13 02:23:24 +0900

    U:8월 13일

--- JS/같은숫자는싫어.js
+++ JS/같은숫자는싫어.js
@@ -19,4 +19,17 @@ function solution(arr) {
 /* 나의 풀이
 Set함수를 이용해서 풀으려고 했는데, 문제점은 같은 숫자는 모두 중복처리가 되기 때문에
 마지막에 연속된 숫자가 나와도 숫자가 반환이 안됐다.
+그래서 고전적인 방법을 사용
+*/
+
+
+/* 다른사람 코드
+function solution(arr)
+{
+    return arr.filter((val,index) => val != arr[index+1]);
+}
+
+1. 필터 사용해서 인자(요소, index)를 사용해서 Boolean값을 이용한다
+2. 요소가 arr[index+1]과 같지 않은 것만 리턴
+와우 ㅋ 어떻게 이런 생각을 완전 간단하게 잘 표현하셨다.. ㅠ
 */
