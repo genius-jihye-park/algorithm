ec168e53110ea067b4d75667d481f546ea76cef7
jihye_Park <sconenomad@gmail.com>
2022-08-14 18:23:59 +0900

    F:8월 14일

---
+++ Python/입출력과사칙연산/FourFund.py
@@ -0,0 +1,33 @@
+# 문제 1000
+# 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+# 8월 14일
+a, b = map(int, input().split())
+print(a+b)
+
+
+# 문제 1001
+# 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+a, b = map(int, input().split())
+print(a-b)
+
+
+# 문제 10998
+# 두 정수 A와 B를 입력받은 다음, A*B를 출력하는 프로그램을 작성하시오.
+a, b = map(int, input().split())
+print(a*b)
+
+
+# 문제 1008
+# 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+a, b = map(int, input().split())
+print(a/b)
+
+
+# 문제 10869
+# 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
+a, b = map(int, input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)  # 단순 나눗셈이 아닌 몫
+print(a % b)
