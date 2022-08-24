96e1af140824e4121ba4de9c1f78452ef8a86ae4
jihye_Park <sconenomad@gmail.com>
2022-08-25 03:27:41 +0900

    8월 25일

--- JS/직사각형별찍기.js
+++ JS/직사각형별찍기.js
@@ -2,3 +2,17 @@
 이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
 별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
 */
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const n = data.split(" ");
+    const a = Number(n[0]), b = Number(n[1]);
+
+    for (let i = 0; i < b; i++) {
+        let str = "";
+        for (let j = 0; j < a; j++) {
+            str = str + "*"
+        }
+        console.log(str)
+    }
+});
