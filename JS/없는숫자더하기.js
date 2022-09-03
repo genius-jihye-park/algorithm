fa64339f94adb480f675148413c5f3b7831153c8
jihye_Park <sconenomad@gmail.com>
2022-09-03 16:16:21 +0900

    F:9월 1일

---
+++ JS/없는숫자더하기.js
@@ -0,0 +1,11 @@
+/*문제 설명
+0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다. 
+numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
+
+*/
+
+function solution(numbers) {
+    let answer = numbers.reduce((pre, cur,) =>
+        pre + cur, 0);
+    return 45 - answer;
+}
