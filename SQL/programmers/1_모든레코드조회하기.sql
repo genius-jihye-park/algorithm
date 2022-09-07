d0e264405b2b169491c0f743dd0bee123beb2cc4
jihye_Park <sconenomad@gmail.com>
2022-09-07 09:11:42 +0900

    UPro:9월 7일

--- SQL/programmers/1_모든레코드조회하기.sql
+++ SQL/programmers/1_모든레코드조회하기.sql
@@ -1,22 +1,11 @@
-/* 문제 설명
-ANIMAL_INS 테이블은 동물 보호소에 들어온 동물의 정보를 담은 테이블입니다. ANIMAL_INS 테이블 구조는 다음과 같으며, ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE는 각각 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부를 나타냅니다.
+-- 문제 설명
+-- ANIMAL_INS 테이블은 동물 보호소에 들어온 동물의 정보를 담은 테이블입니다.
+--  ANIMAL_INS 테이블 구조는 다음과 같으며, ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE는 
+--  각각 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부를 나타냅니다.
 
-NAME	TYPE	NULLABLE
-ANIMAL_ID	VARCHAR(N)	FALSE
-ANIMAL_TYPE	VARCHAR(N)	FALSE
-DATETIME	DATETIME	FALSE
-INTAKE_CONDITION	VARCHAR(N)	FALSE
-NAME	VARCHAR(N)	TRUE
-SEX_UPON_INTAKE	VARCHAR(N)	FALSE
-동물 보호소에 들어온 모든 동물의 정보를 ANIMAL_ID순으로 조회하는 SQL문을 작성해주세요. SQL을 실행하면 다음과 같이 출력되어야 합니다.
 
-ANIMAL_ID	ANIMAL_TYPE	DATETIME	INTAKE_CONDITION	NAME	SEX_UPON_INTAKE
-A349996	Cat	2018-01-22 14:32:00	Normal	Sugar	Neutered Male
-A350276	Cat	2017-08-13 13:50:00	Normal	Jewel	Spayed Female
-A350375	Cat	2017-03-06 15:01:00	Normal	Meo	Neutered Male
-A352555	Dog	2014-08-08 04:20:00	Normal	Harley	Spayed Female
-..이하 생략
-*/
+-- 동물 보호소에 들어온 모든 동물의 정보를 ANIMAL_ID순으로 조회하는 SQL문을 작성해주세요. SQL을 실행하면 다음과 같이 출력되어야 합니다.
+
 
 -- 나의 정답
 
--- SQL/programmers/3_아픈동물찾기.sql
+++ SQL/programmers/3_아픈동물찾기.sql
@@ -1,29 +1,12 @@
-/* 문제 설명
-ANIMAL_INS 테이블은 동물 보호소에 들어온 동물의 정보를 담은 테이블입니다. ANIMAL_INS 테이블 구조는 다음과 같으며, ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE는 각각 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부를 나타냅니다.
+-- 문제 설명
+-- ANIMAL_INS 테이블은 동물 보호소에 들어온 동물의 정보를 담은 테이블입니다. 
+-- ANIMAL_INS 테이블 구조는 다음과 같으며, ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE는 
+-- 각각 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부를 나타냅니다.
 
-NAME	TYPE	NULLABLE
-ANIMAL_ID	VARCHAR(N)	FALSE
-ANIMAL_TYPE	VARCHAR(N)	FALSE
-DATETIME	DATETIME	FALSE
-INTAKE_CONDITION	VARCHAR(N)	FALSE
-NAME	VARCHAR(N)	TRUE
-SEX_UPON_INTAKE	VARCHAR(N)	FALSE
-동물 보호소에 들어온 동물 중 아픈 동물1의 아이디와 이름을 조회하는 SQL 문을 작성해주세요. 이때 결과는 아이디 순으로 조회해주세요.
+-- 동물 보호소에 들어온 동물 중 아픈 동물1의 아이디와 이름을 조회하는 
+-- SQL 문을 작성해주세요. 이때 결과는 아이디 순으로 조회해주세요.
 
-예시
-예를 들어 ANIMAL_INS 테이블이 다음과 같다면
 
-ANIMAL_ID	ANIMAL_TYPE	DATETIME	INTAKE_CONDITION	NAME	SEX_UPON_INTAKE
-A365172	Dog	2014-08-26 12:53:00	Normal	Diablo	Neutered Male
-A367012	Dog	2015-09-16 09:06:00	Sick	Miller	Neutered Male
-A365302	Dog	2017-01-08 16:34:00	Aged	Minnie	Spayed Female
-A381217	Dog	2017-07-08 09:41:00	Sick	Cherokee	Neutered Male
-이 중 아픈 동물은 Miller와 Cherokee입니다. 따라서 SQL문을 실행하면 다음과 같이 나와야 합니다.
-
-ANIMAL_ID	NAME
-A367012	Miller
-A381217	Cherokee
-*/
 ---- 코드를 입력하세요
 SELECT ANIMAL_ID, NAME
 FROM ANIMAL_INS
